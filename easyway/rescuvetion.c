4d52804305f5a57632a10afe25bb8181ec8376ca
Efe Burak Ostundag <129344217+cavemanbob@users.noreply.github.com>
2023-11-29 20:24:45 +0300

    Create rescuvetion.c

---
+++ easyway/rescuvetion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int f2(int bn, int dec=0, int rcx=0){
+	return bn==0 ? dec : f2(bn/10, dec+ (int)pow(2,rcx) * bn%10, rcx+1);
+}
+
+int main(){
+  printf("%d",f2(1001001));
+  return 0;
+}
